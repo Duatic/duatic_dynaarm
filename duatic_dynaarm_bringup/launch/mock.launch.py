a34d72edb19e596469eea2eb8770c93f12b1cfe9
svenkae1234 <sven.kaehler@freenet.de>
2026-05-22 11:16:46 +0200

    Added new dynaarm version

--- duatic_dynaarm_bringup/launch/mock.launch.py
+++ duatic_dynaarm_bringup/launch/mock.launch.py
@@ -151,7 +151,7 @@ def generate_launch_description():
         DeclareLaunchArgument(
             name="version",
             default_value="baracuda12",
-            choices=["arowana4", "baracuda12"],
+            choices=["arowana4", "baracuda12", "corydoras12"],
             description="Select the desired version of robot ",
         ),
         DeclareLaunchArgument(
