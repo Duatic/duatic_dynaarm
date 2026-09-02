d5242096f9ecfc7003b4ff0856351176effcd2ec
Patrick Vonwirth <pvonwirth@duatic.com>
2026-09-02 15:13:54 +0200

    bugfix launch parameter

--- duatic_dynaarm_bringup/launch/real.launch.py
+++ duatic_dynaarm_bringup/launch/real.launch.py
@@ -97,7 +97,7 @@ def launch_setup(context, *args, **kwargs):
             Node(
                 package="controller_manager",
                 executable="ros2_control_node",
-                parameters=controllers_params,
+                parameters=[controllers_params],
                 output={
                     "stdout": "screen",
                     "stderr": "screen",
